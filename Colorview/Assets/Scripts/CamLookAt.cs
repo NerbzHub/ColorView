86b6a49af7b51ce8e588b646fc955f761fe9059c
NerbzHub <nathan.nette@gmail.com>
2018-06-04 09:30:06 +1000

    Added the game so far: player can move and camera looks at the enemy but player doesn't stay in the middle of the view.

---
+++ Colorview/Assets/Scripts/CamLookAt.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamLookAt : MonoBehaviour
+{
+
+    public Camera mainCam;
+    public GameObject bigBoss;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.LookAt(bigBoss.transform);
+    }
+}
